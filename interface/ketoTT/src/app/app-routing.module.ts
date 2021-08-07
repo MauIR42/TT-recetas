f5ab410e4a5b9805bd6a54c564476eb40377b2db
mauricio.romero <mauricio.romero@go-sharp.com>
2021-08-07 18:44:24 -0500

    sección de báscula en el menú inventario, html de todos los casos

--- interface/ketoTT/src/app/app-routing.module.ts
+++ interface/ketoTT/src/app/app-routing.module.ts
@@ -5,19 +5,21 @@ import { LoginComponent } from './login/login/login.component';
 import { RecoveryPasswordComponent } from './components/recovery-password/recovery-password.component';
 import { HeaderComponent } from './components/header/header.component';
 import { RegisterComponent } from './components/register/register.component';
+import { StockComponent } from './components/stock/stock.component'
 
 const routes: Routes = [
   { path:'test', component: HeaderComponent },
   { path:'perfil', component: HeaderComponent },
   { path:'estadisticas', component: HeaderComponent },
-  { path:'inventario', component: HeaderComponent },
-  { path:'bascula', component: HeaderComponent },
+  { path:'inventario', component: StockComponent },
+  { path:'bascula', component: StockComponent },
   { path:'planeacion_semanal', component: HeaderComponent },
   { path:'dieta_keto', component: HeaderComponent },
   { path:'sindrome_metabolico', component: HeaderComponent },
   { path:'recuperar_contraseña', component: RecoveryPasswordComponent },
   { path:'crear_cuenta', component: RegisterComponent },
   { path: '', component: LoginComponent },
+  { path: '**', component: LoginComponent },
 
 ];
 
--- interface/ketoTT/src/app/components/stock/stock.component.ts
+++ interface/ketoTT/src/app/components/stock/stock.component.ts
@@ -7,9 +7,75 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StockComponent implements OnInit {
 
+  user_type :number = 3;
+
+  type_info : any = {
+    1 : {'name':'Sin báscula', form_use:true, text: 1 },
+    2 : {'name':'Con báscula', form_use:false, text:2},
+    3 : {'name':'Administrador', form_use:false, text:2},
+    4 : {'name':'Sin báscula', form_use:true, text: 1},
+    5 : {'name':'Sin báscula', form_use:true, text: 1},
+  }
+
+  scale_users : any = [
+    {
+      'name': 'Martin Perez',
+      'id': 504,
+      'pending' : true
+    },
+    {
+      'name': 'Juanita Perez',
+      'id': 505,
+      'pending' : false
+    },
+    {
+      'name': 'Marcos Perez',
+      'id': 506,
+      'pending' : true
+    },
+    {
+      'name': 'Karen Perez',
+      'id': 507,
+      'pending' : false
+    },
+
+  ];
+
+  current_users : any = [
+    {
+        'name': 'Juanita Perez',
+        'id': 505,
+      },
+    {
+        'name': 'Karen Perez',
+        'id': 507,
+      },
+  ]
+
+  associated_scale : string = "xxx-xxx";
+
+  accept_user(event: any, user_id: number){
+    event.preventDefault();
+    console.log("aceptar usuario", user_id);
+  }
+
+  delete_user(event: any, user_id: number){
+    event.preventDefault();+
+    console.log("eliminar usuario", user_id);
+  }
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  change_user_type(event : any){
+    console.log("listo!");
+    event.preventDefault();
+  }
+
+  check(){
+    console.log("se revisa");
   }
 
 }
