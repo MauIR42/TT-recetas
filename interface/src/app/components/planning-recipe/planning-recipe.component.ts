ff82c24bf911ff9bc1ed7d64b3289293b941355c
Mauricio Romero <mromerop1301@alumno.ipn.mx>
2021-09-23 22:37:13 -0500

    add style to prepare mode

--- interface/src/app/components/planning-recipe/planning-recipe.component.ts
+++ interface/src/app/components/planning-recipe/planning-recipe.component.ts
@@ -17,11 +17,13 @@ export class PlanningRecipeComponent implements OnInit {
       'start': moment().startOf('week').add(7 ,'d'),
       'end'  : moment().endOf('week').add(7 ,'d'),
       'next' : 'Preparación',
+      'date_string' : ''
     },
     'Preparación': {
       'start': moment().startOf('week'),
       'end'  : moment().endOf('week'),
       'next' : 'Planeación',
+      'date_string': ''
     }
   }
 
@@ -40,16 +42,47 @@ export class PlanningRecipeComponent implements OnInit {
 
   delete_img = 'x-circle-fill-orange.svg';
 
-  range_string : string = 'arrow-left-white.svg';
+  range_string : string = '';
 
-  to_make_recipes = [
-    [{'name':'abcde', 'id':15},{'name':'abcde', 'id':16},{'name':'abcde', 'id':17},{'name':'abcde', 'id':18}],
-    [{'name':'abcde', 'id':15},{'name':'abcde', 'id':16},{'name':'abcde', 'id':17},{'name':'abcde', 'id':18},{'name':'abcde', 'id':19},{'name':'abcde', 'id':20},{'name':'abcde', 'id':21},{'name':'abcde', 'id':22}],
-    [{'name':'abcde', 'id':15},{'name':'abcde', 'id':16},{'name':'abcde', 'id':17},{'name':'abcde', 'id':18}],
-    [{'name':'abcde', 'id':15},{'name':'abcde', 'id':16},{'name':'abcde', 'id':17},{'name':'abcde', 'id':18},{'name':'abcde', 'id':19},{'name':'abcde', 'id':20},{'name':'abcde', 'id':21},{'name':'abcde', 'id':22}],
-    [{'name':'abcde', 'id':15},{'name':'abcde', 'id':16},{'name':'abcde', 'id':17},{'name':'abcde', 'id':18}],
-    [{'name':'abcde', 'id':15},{'name':'abcde', 'id':16},{'name':'abcde', 'id':17},{'name':'abcde', 'id':18},{'name':'abcde', 'id':19},{'name':'abcde', 'id':20},{'name':'abcde', 'id':21},{'name':'abcde', 'id':22}],
-    [{'name':'abcde', 'id':15},{'name':'abcde', 'id':16},{'name':'abcde', 'id':17},{'name':'abcde', 'id':18}],
+  to_make_recipes : any = [
+    [
+      // {'name':'receta 1', 'id_recipe':15, 'duration':20, 'cooked':5, 'ingredient_percentage':100, 'image':"assets/images/test1.jpg", 'id_user_recipe':15, 'description': "Esta es una breve descripción de la receta para que el usuario recuerde que habíadsuiwEVBWEUBVUEOWVBWVBDBDSBVODSBBhhrpvfibnvifbnibnsdvvwebvebnhml.,mhnbgfvdc"},
+      // {'name':'receta 2', 'id_recipe':15, 'duration':30, 'cooked':3, 'ingredient_percentage':80, 'image':"assets/images/test1.jpg", 'id_user_recipe':16, 'description': "Esta es una breve descripción de la receta para que el usuario recuerde que había."},
+      // {'name':'receta 3', 'id_recipe':15, 'duration':40, 'cooked':2, 'ingredient_percentage':75, 'image':"assets/images/test1.jpg", 'id_user_recipe':17, 'description': "Esta es una breve descripción de la receta para que el usuario recuerde que había."},
+      // {'name':'receta 4', 'id_recipe':15, 'duration':70, 'cooked':18, 'ingredient_percentage':60, 'image':"assets/images/test1.jpg", 'id_user_recipe':18, 'description': "Esta es una breve descripción de la receta para que el usuario recuerde que había."},
+      // {'name':'receta 5', 'id_recipe':15, 'duration':70, 'cooked':18, 'ingredient_percentage':58, 'image':"assets/images/test1.jpg", 'id_user_recipe':19, 'description': "Esta es una breve descripción de la receta para que el usuario recuerde que había."},
+    ],[
+      // {'name':'receta 6', 'id_recipe':15, 'duration':20, 'cooked':5, 'ingredient_percentage':59, 'image':"assets/images/test1.jpg", 'id_user_recipe':20, 'description': "Esta es una breve descripción de la receta para que el usuario recuerde que había."},
+      // {'name':'receta 7', 'id_recipe':15, 'duration':30, 'cooked':3, 'ingredient_percentage':38, 'image':"assets/images/test1.jpg", 'id_user_recipe':21, 'description': "Esta es una breve descripción de la receta para que el usuario recuerde que había."},
+      // {'name':'receta 8', 'id_recipe':15, 'duration':40, 'cooked':2, 'ingredient_percentage':20, 'image':"assets/images/test1.jpg", 'id_user_recipe':22, 'description': "Esta es una breve descripción de la receta para que el usuario recuerde que había."},
+      // {'name':'receta 9', 'id_recipe':15, 'duration':70, 'cooked':18, 'ingredient_percentage':15, 'image':"assets/images/test1.jpg", 'id_user_recipe':23, 'description': "Esta es una breve descripción de la receta para que el usuario recuerde que había."},
+      // {'name':'receta 10', 'id_recipe':15, 'duration':70, 'cooked':18, 'ingredient_percentage':0, 'image':"assets/images/test1.jpg", 'id_user_recipe':24, 'description': "Esta es una breve descripción de la receta para que el usuario recuerde que había."},
+    ],[
+      // {'name':'receta 1', 'id_recipe':15, 'duration':20, 'cooked':5, 'ingredient_percentage':100, 'image':"assets/images/test1.jpg", 'id_user_recipe':25, 'description': "Esta es una breve descripción de la receta para que el usuario recuerde que había."},
+      // {'name':'receta 2', 'id_recipe':15, 'duration':30, 'cooked':3, 'ingredient_percentage':80, 'image':"assets/images/test1.jpg", 'id_user_recipe':26, 'description': "Esta es una breve descripción de la receta para que el usuario recuerde que había."},
+      // {'name':'receta 3', 'id_recipe':15, 'duration':40, 'cooked':2, 'ingredient_percentage':75, 'image':"assets/images/test1.jpg", 'id_user_recipe':27, 'description': "Esta es una breve descripción de la receta para que el usuario recuerde que había."},
+      // {'name':'receta 4', 'id_recipe':15, 'duration':70, 'cooked':18, 'ingredient_percentage':60, 'image':"assets/images/test1.jpg", 'id_user_recipe':28, 'description': "Esta es una breve descripción de la receta para que el usuario recuerde que había."},
+      // {'name':'receta 5', 'id_recipe':15, 'duration':70, 'cooked':18, 'ingredient_percentage':58, 'image':"assets/images/test1.jpg", 'id_user_recipe':29, 'description': "Esta es una breve descripción de la receta para que el usuario recuerde que había."},
+    ],[
+      // {'name':'receta 6', 'id_recipe':15, 'duration':20, 'cooked':5, 'ingredient_percentage':59, 'image':"assets/images/test1.jpg", 'id_user_recipe':30, 'description': "Esta es una breve descripción de la receta para que el usuario recuerde que había."},
+      // {'name':'receta 7', 'id_recipe':15, 'duration':30, 'cooked':3, 'ingredient_percentage':38, 'image':"assets/images/test1.jpg", 'id_user_recipe':31, 'description': "Esta es una breve descripción de la receta para que el usuario recuerde que había."},
+      // {'name':'receta 8', 'id_recipe':15, 'duration':40, 'cooked':2, 'ingredient_percentage':20, 'image':"assets/images/test1.jpg", 'id_user_recipe':32, 'description': "Esta es una breve descripción de la receta para que el usuario recuerde que había."},
+      // {'name':'receta 9', 'id_recipe':15, 'duration':70, 'cooked':18, 'ingredient_percentage':15, 'image':"assets/images/test1.jpg", 'id_user_recipe':33, 'description': "Esta es una breve descripción de la receta para que el usuario recuerde que había."},
+      // {'name':'receta 10', 'id_recipe':15, 'duration':70, 'cooked':18, 'ingredient_percentage':0, 'image':"assets/images/test1.jpg", 'id_user_recipe':34, 'description': "Esta es una breve descripción de la receta para que el usuario recuerde que había."},
+    ],[
+      // {'name':'receta 1', 'id_recipe':15, 'duration':20, 'cooked':5, 'ingredient_percentage':100, 'image':"assets/images/test1.jpg", 'id_user_recipe':35, 'description': "Esta es una breve descripción de la receta para que el usuario recuerde que había."},
+      // {'name':'receta 2', 'id_recipe':15, 'duration':30, 'cooked':3, 'ingredient_percentage':80, 'image':"assets/images/test1.jpg", 'id_user_recipe':36, 'description': "Esta es una breve descripción de la receta para que el usuario recuerde que había."},
+      // {'name':'receta 3', 'id_recipe':15, 'duration':40, 'cooked':2, 'ingredient_percentage':75, 'image':"assets/images/test1.jpg", 'id_user_recipe':37, 'description': "Esta es una breve descripción de la receta para que el usuario recuerde que había."},
+      // {'name':'receta 4', 'id_recipe':15, 'duration':70, 'cooked':18, 'ingredient_percentage':60, 'image':"assets/images/test1.jpg", 'id_user_recipe':38, 'description': "Esta es una breve descripción de la receta para que el usuario recuerde que había."},
+      // {'name':'receta 5', 'id_recipe':15, 'duration':70, 'cooked':18, 'ingredient_percentage':58, 'image':"assets/images/test1.jpg", 'id_user_recipe':39, 'description': "Esta es una breve descripción de la receta para que el usuario recuerde que había."},
+    ],[
+      // {'name':'receta 6', 'id_recipe':15, 'duration':20, 'cooked':5, 'ingredient_percentage':59, 'image':"assets/images/test1.jpg", 'id_user_recipe':40, 'description': "Esta es una breve descripción de la receta para que el usuario recuerde que había."},
+      // {'name':'receta 7', 'id_recipe':15, 'duration':30, 'cooked':3, 'ingredient_percentage':38, 'image':"assets/images/test1.jpg", 'id_user_recipe':41, 'description': "Esta es una breve descripción de la receta para que el usuario recuerde que había."},
+      // {'name':'receta 8', 'id_recipe':15, 'duration':40, 'cooked':2, 'ingredient_percentage':20, 'image':"assets/images/test1.jpg", 'id_user_recipe':42, 'description': "Esta es una breve descripción de la receta para que el usuario recuerde que había."},
+      // {'name':'receta 9', 'id_recipe':15, 'duration':70, 'cooked':18, 'ingredient_percentage':15, 'image':"assets/images/test1.jpg", 'id_user_recipe':43, 'description': "Esta es una breve descripción de la receta para que el usuario recuerde que había."},
+      // {'name':'receta 10', 'id_recipe':15, 'duration':70, 'cooked':18, 'ingredient_percentage':0, 'image':"assets/images/test1.jpg", 'id_user_recipe':44, 'description': "Esta es una breve descripción de la receta para que el usuario recuerde que había."},
+    ],[
+    ],
   ];
 
   planning_recipes = [
@@ -62,6 +95,8 @@ export class PlanningRecipeComponent implements OnInit {
     [],
   ];
 
+
+
   actual_modal : any;
 
   show_modal : boolean = false;
@@ -71,7 +106,12 @@ export class PlanningRecipeComponent implements OnInit {
   constructor(private spinner: NgxSpinnerService) { }
 
   ngOnInit(): void {
-    this.range_string = 'del ' + this.menu[ this.current ]['start'].format("DD") + ' al ' +this.menu[ this.current ]['end'].locale('es') .format("DD [de] MMMM  [del] YYYY");
+
+    this.menu['Planeación']['date_string'] = 'del ' + this.menu[ 'Planeación' ]['start'].format("DD") + ' al ' +this.menu[ 'Planeación' ]['end'].locale('es') .format("DD [de] MMMM  [del] YYYY");;
+
+    this.menu['Preparación']['date_string'] = 'del ' + this.menu[ 'Preparación' ]['start'].format("DD") + ' al ' +this.menu[ 'Preparación' ]['end'].locale('es') .format("DD [de] MMMM  [del] YYYY");
+
+    this.range_string = this.menu[this.current]['date_string'];
     // $(document).ready(function() {
     //   $("#planning_modal").modal("show");
     // });
@@ -82,7 +122,7 @@ export class PlanningRecipeComponent implements OnInit {
     if(event)
       event.preventDefault();
     console.log(element);
-    this.current_recipe = element['id'];
+    this.current_recipe = element['id_user_recipe'];
   }
 
   change_day(type: number){
@@ -95,7 +135,7 @@ export class PlanningRecipeComponent implements OnInit {
   change_menu(next : string){
     // let aux = this.current;
     this.current = next;
-    this.range_string = 'del ' + this.menu[ this.current ]['start'].format("DD") + ' al ' +this.menu[ this.current ]['end'].locale('es') .format("DD [de] MMMM  [del] YYYY");
+    this.range_string = this.menu[this.current]['date_string'];
     // this.next = aux;
   }
 
